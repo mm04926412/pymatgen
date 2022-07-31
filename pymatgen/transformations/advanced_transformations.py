7a7e9a5c17a93b7bd0f1ac070efc8e9a0889ba84
Michael Moran <mm04926412@gmail.com>
2022-07-31 18:08:33 +0100

    initial commit

--- pymatgen/transformations/advanced_transformations.py
+++ pymatgen/transformations/advanced_transformations.py
@@ -17,6 +17,7 @@ from string import ascii_lowercase
 from typing import Iterable
 
 import numpy as np
+import scipy as sp
 from monty.dev import requires
 from monty.fractions import lcm
 from monty.json import MSONable
@@ -2223,4 +2224,70 @@ class MonteCarloRattleTransformation(AbstractTransformation):
         """
         Returns: False
         """
+        return False
+
+class OrthorhombicSupercellTransform(AbstractTransformation):
+    """
+    This transformation generates a combined scaled and shear of the provided unit cell to achieve a roughly
+    OrthorhomicSupercell. 
+    """
+
+    def __init__(self, N_Atoms):
+        """
+        Args:
+            charge_balance_sp: The desired number of atoms in the supercell
+        """
+        self.N_Atoms = int(N_Atoms)
+
+    def apply_transformation(self, structure):
+        """
+        Applies the transformation.
+
+        Args:
+            structure: Input Structure
+
+        Returns:
+            OrthorhombicSupercell
+        """
+
+        lattice_matrix = structure.lattice.as_dict()["matrix"]
+
+        #RQ decomposition in this context provides a scale and shear matrix (R) that maps a Orthorhombic cell of unit volume to the current lattice parameters
+        R, Q = sp.linalg.rq(lattice_matrix)
+        #Invert R to get the scale+shear that maps the current unit cell to the Orthorhombic cell
+        R1 = sp.inv(R)
+        #Compute the inverse of R1's diagonal and apply as a matrix multplication to remove scaling component from R1, as this is undesirable
+        R1_Diagonal = np.zeros(R1.shape)
+        np.fill_diagonal(R1_Diagonal,np.diagonal(R1))
+        #S is the 'ideal' normalized shearing, it is not yet suitable due to its non-integer components
+        S = sp.inv(R1_Diagonal) @ R1
+
+        #The lattice parameters of Q are the "ideal" attained by directly applying S, we compute our scaling matrix by iteratively incrementing the shortest lattice parameter on Q
+        #until any further increments breach the upper atom limit. These increments on Q are used to compute the scaling component of the transformation
+        start_len = len(struct)
+        Q_abc = [np.linalg.norm(Q[0]),np.linalg.norm(Q[1]),np.linalg.norm(Q[2])]
+        increments = (1,1,1)
+        found_transform = False
+        while not found_transform:
+            new_increments = list(increments) #Deep copy
+            shortest = np.argmin([i*j for i,j in zip(increments,Q_abc)]) #Return the shortest lattice parameter of Q post scaling
+            new_increments[shortest] += 1
+            if np.prod(new_increments)*start_len <= self.N_Atoms: #If this increment brings the total number of atoms above the ceiling then return the transformation matrix, otherwise repeat
+                increments = new_increments
+            else:
+                found_transform=True
+        cubic_upscale_approx = np.rint(np.diag(increments) @ S) # Create combined scale and shear matrix and round the off diagonals to the nearest integer, this provides an integer approximation of the shear, larger supercells will be more precise
+
+        struct = SupercellTransformation(scaling_matrix=cubic_upscale_approx).apply_transformation(struct) # Apply the computed integer supercell transformation
+        return struct
+
+
+    @property
+    def inverse(self):
+        """Returns: None"""
+        return None
+
+    @property
+    def is_one_to_many(self):
+        """Returns: False"""
         return False
